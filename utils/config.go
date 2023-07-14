06a5e5d8b427083887dd9cee98c84b93dfb9f243
Diego Araujo <diegolaraujo96@gmail.com>
2023-07-14 14:27:51 -0300

    Added external bots functions

--- utils/config.go
+++ utils/config.go
@@ -18,6 +18,10 @@ var Servers map[string]string
 
 var Secondary map[string]string
 
+var Telegram_bot_token string
+var Whatsapp_bot_token string
+var Whatsapp_endpoint string
+
 func ReadConfig() error {
 
 	file, err := ioutil.ReadFile("./config.json")
@@ -34,6 +38,9 @@ func ReadConfig() error {
         Jira_user  string `json:"Jira_user"`
         Url        string `json:"Url"`
         Healthcheck map[string]string `json:"Healthcheck"`
+        Telegram_bot_token string `json:"Telegram_bot_token"`
+        Whatsapp_bot_token string `json:"Whatsapp_bot_token"`
+        Whatsapp_endpoint string `json:"Whatsapp_endpoint"`
     }
 
 	json.Unmarshal(file, &config)
@@ -45,6 +52,9 @@ func ReadConfig() error {
     Url = config.Url
     BotToken = config.Token
     Servers = config.Healthcheck
+    Telegram_bot_token = config.Telegram_bot_token
+    Whatsapp_bot_token = config.Whatsapp_bot_token
+    Whatsapp_endpoint = config.Whatsapp_endpoint
 
 	return err
 }
